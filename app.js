59dd532b8501d715a42318f1c07692bf27672528
Santanu Garai <santanugarai2015@gmail.com>
2025-07-22 01:29:48 +0530

    base commit

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello From Cozeva!");
+    res.send(
+        "Hello From ARW! Here The Docker Image build on Jenkins server and pushed to Docker Hub. then Deployed on Application Server"
+    );
 });
 
 app.listen(port, () => {
